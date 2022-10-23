29c12808fb51a243fb9ab786654f14251eb9aac0
germanFid <german.fidarov@gmail.com>
2022-10-23 23:07:11 +0300

    Add win32 compatibility

--- Varya_Struct_02/tmp_main.c
+++ Varya_Struct_02/tmp_main.c
@@ -1,5 +1,59 @@
 #include "parcer.h"
 
+#ifdef _WIN32
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL) {
+        return -1;
+    }
+    if (stream == NULL) {
+        return -1;
+    }
+    if (n == NULL) {
+        return -1;
+    }
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+    if (bufptr == NULL) {
+        bufptr = malloc(128);
+        if (bufptr == NULL) {
+            return -1;
+        }
+        size = 128;
+    }
+    p = bufptr;
+    while(c != EOF) {
+        if ((p - bufptr) > (size - 1)) {
+            size = size + 128;
+            bufptr = realloc(bufptr, size);
+            if (bufptr == NULL) {
+                return -1;
+            }
+        }
+        *p++ = c;
+        if (c == '\n') {
+            break;
+        }
+        c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+#endif
+
 int main(int argc, char** argv)
 {
     FILE* IFile = NULL;
@@ -27,10 +81,11 @@ int main(int argc, char** argv)
         }
 
         while ((read = getline(&ILine, &len, IFile)) != -1) {
-        // printf("Retrieved line of length %zu:\n", read);
-        printf("%s", ILine);
-        parceLine(ILine);
+            // printf("Retrieved line of length %zu:\n", read);
+            printf("%s", ILine);
+            parceLine(ILine);
         }
+        printDataBase();
 
         fclose(IFile);
         if (ILine)
